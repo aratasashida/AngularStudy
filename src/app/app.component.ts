d829cce551be54827b49ac27f8e4e5870bf63459
arata.sashida <art52wind@gmail.com>
2020-11-14 02:00:45 +0900

    http通信でgetしたリストを画面描画まで行った

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularAfterTutorial';
+export class AppComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http
+      .get<{ data: User[] }>('https://reqres.in/api/users')
+      .subscribe(resp => {
+        console.log(resp);
+        this.users = resp.data;
+      });
+  }
 }
