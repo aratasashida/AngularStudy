9e8755ce800544ff9cca5d768c8fa6c4db8ead40
arata.sashida <art52wind@gmail.com>
2020-12-15 01:50:00 +0900

    [WIP] Single State Streamに沿って実装

--- src/app/components/study-rxjs/asyncpipe/asyncpipe.component.ts
+++ src/app/components/study-rxjs/asyncpipe/asyncpipe.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
 import { DataService } from '../../../service/data.service';
 
+type State = {
+  value: any;
+};
 @Component({
   selector: 'app-asyncpipe',
   templateUrl: './asyncpipe.component.html',
   styleUrls: ['./asyncpipe.component.scss']
 })
 export class AsyncpipeComponent {
-  value$: Observable<any>;
+  // Single State Stream
+  readonly state$: Observable<State>;
 
   constructor(private dataService: DataService) {
-    this.value$ = this.dataService.valueChanges;
+    this.state$ = combineLatest(
+        [this.dataService.valueChanges], // 必要なストリームを合成する
+        ([value]) => ({ value }), // 配列からオブジェクトに変換する
+    );
   }
-
 }
